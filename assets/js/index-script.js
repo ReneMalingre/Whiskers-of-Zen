c72c6048fc6de772134233b31d06a4fe8ecce864
Rene Malingre <rene.malingre@gmail.com>
2023-05-02 15:51:41 +0930

    Fine tuning modal on favourites and formatting on index.html

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -1,36 +1,36 @@
-//Enable format to array
-const landingdata = {
+// Enable format to array
+const landingData = {
   dogs: 5,
-cats: 5,
-mood: 3
+  cats: 5,
+  mood: 3,
 };
 
-//Save Mood to Local storage
-const initialmood = document.getElementById('zen-level');
-initialmood.addEventListener('change', (event) => {
-landingdata.mood = event.target.value;
-console.log(landingdata.mood);
+// Save Mood to Local storage
+const initialMood = document.getElementById('zen-level');
+initialMood.addEventListener('change', (event) => {
+  landingData.mood = event.target.value;
+  console.log(landingData.mood);
 });
 
-//Save dropdown for number of dogs go local storage
-const dognumber = document.getElementById("image-number-dogs");
-dognumber.addEventListener('change', () => {
- landingdata.dogs = event.target.value;
-  console.log(landingdata.dogs);
+// Save dropdown for number of dogs go local storage
+const dogNumber = document.getElementById('image-number-dogs');
+dogNumber.addEventListener('change', () => {
+  landingData.dogs = event.target.value;
+  console.log(landingData.dogs);
 });
 
-//Save dropdown for number of cats to local storage
-const catnumber = document.getElementById("image-number-cats");
-catnumber.addEventListener('change', () => {
-  landingdata.cats = event.target.value;
-  console.log(landingdata.cats);
+// Save dropdown for number of cats to local storage
+const catNumber = document.getElementById('image-number-cats');
+catNumber.addEventListener('change', () => {
+  landingData.cats = event.target.value;
+  console.log(landingData.cats);
 });
 
-//Load your Furry Friends Button to take to app page
-const submitbutton = document.getElementById('submit-button');
-submitbutton.addEventListener('click', () => {
+// Load your Furry Friends Button to take to app page
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', () => {
 // create querystring
-const queryString = new URLSearchParams(landingdata).toString();
-console.log(queryString);
-window.location.href = 'app.html?' + queryString; 
+  const queryString = new URLSearchParams(landingData).toString();
+  console.log(queryString);
+  window.location.href = 'app.html?' + queryString;
 });
