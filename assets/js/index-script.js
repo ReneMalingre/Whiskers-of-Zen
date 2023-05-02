929e99e9e813d3445abe2327b7b92ea1ad8d5669
Sebastianrod8 <126779658+Sebastianrod8@users.noreply.github.com>
2023-05-02 19:44:59 +0930

    music controls added and js code modified

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -2,6 +2,7 @@
 const submitButton = document.getElementById('submit-button');
 // add event listener to submit button
 submitButton.addEventListener('click', submitForm);
+const achievementsSelector=document.getElementById('itHadBetterWorkThisTime');
 
 function submitForm() {
   // get form data here
@@ -10,3 +11,21 @@ function submitForm() {
   const queryString = 'dogs=4&cats=4&mood=1&name=Bob';
   window.location.href = `app.html?${queryString}`;
 }
+
+//music function
+let audioClass = new Audio('./assets/music/Mandeville.mp3');
+
+
+ const toggleMusic = document.querySelector('#toggle-music');
+
+ toggleMusic.addEventListener('change', () => {
+  console.log('change event fired');
+  if (toggleMusic.checked) {
+    console.log('checked = true');
+    audioClass.play();
+  } else {
+    console.log('checked = false');
+    audioClass.pause();
+  }
+ }); 
+
